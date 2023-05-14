4873eb8fecd0afd2671e571689cfe04583dbbe72
MaritaIfemeni <marita.ifemeni@gmail.com>
2023-05-14 16:31:12 +0300

    form prac

--- src/pages/LogInPage.tsx
+++ src/pages/LogInPage.tsx
@@ -1,29 +1,35 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
 const LogInPage = () => {
+  const { handleSubmit, register } =useForm();
+  const onSubmit = (data: any) => {
+    console.log(data);
+  };
+
   return (
     <div>
       LogIn registration form
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <div>
           <label htmlFor="username">Userame</label>
-          <input type="text" name="username" id="username" />
+          <input type="text" id="username" {...register("username")} />
         </div>
         <div>
           <label htmlFor="email">Email</label>
-          <input type="email" name="email" id="email" />
+          <input type="email" id="email" {...register("email")}/>
         </div>
         <div>
           <label htmlFor="phone">Phone</label>
-          <input type="phone" name="phone" id="phone" />
+          <input type="phone" id="phone" {...register("phone")}/>
         </div>
         <div>
           <label htmlFor="password">Password</label>
-          <input type="password" name="password" id="password" />
+          <input type="password" id="password" {...register("password")}/>
         </div>
         <div>
           <label htmlFor="confirm">Password Again</label>
-          <input type="password" name="confirm" id="confirm" />
+          <input type="password" id="confirm" {...register("confirm")}/>
         </div>
         <button type="submit">Submit</button>
       </form>
